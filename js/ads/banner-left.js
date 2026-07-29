1c0f0da99e356e382519e8dd652d2c2d238c916e
ManuExplora <libertad28j2024@gmail.com>
2026-07-29 13:24:15 -0300

    feat(monetization): Migración táctica de flancos laterales a red A-Ads (Anonymous Ads - Unit 2449857) con HTTPS y blindaje sandbox #3

--- js/ads/banner-left.js
+++ js/ads/banner-left.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
       <!-- BEGIN AADS AD UNIT 2449857 -->
         <div id="frame" style="width: 100%;margin: auto;position: relative; z-index: 99998;">
           <iframe data-aa='2449857' src='//acceptable.a-ads.com/2449857/?size=Adaptive'
-            style='border:0; padding:0; width:100%; height:auto; overflow:hidden;display: block;margin: auto'></iframe>
+            style='border:0; padding:0; width:70%; height:auto; overflow:hidden;display: block;margin: auto'></iframe>
         </div>
       <!-- END AADS AD UNIT 2449857 -->
     `;
