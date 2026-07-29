234146e4b49eb2d5c427375d35a2b63eb43aa483
ManuExplora <libertad28j2024@gmail.com>
2026-07-29 13:29:11 -0300

    feat(monetization): Migración táctica de flancos laterales a red A-Ads top y bottom  #4

--- js/ads/banner-bottom.js
+++ js/ads/banner-bottom.js
@@ -6,20 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
   if (container) {
     // Inyectamos el iframe aislado
     container.innerHTML += `
-      <iframe sandbox="allow-scripts allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-forms" srcdoc="...
-        <html><head></head><body style='margin:0;padding:0;display:flex;justify-content:center;align-items:center;background:transparent;'>
-          <script>
-            atOptions = {
-              'key' : 'fb4441ee10a2b52843e9600d293de6f1',
-              'format' : 'iframe',
-              'height' : 90,
-              'width' : 728,
-              'params' : {}
-            };
-          </script>
-          <script src='https://www.highperformanceformat.com/fb4441ee10a2b52843e9600d293de6f1/invoke.js'></script>
-        </body></html>
-      " style="width:728px; height:90px; border:none; overflow:hidden; position:relative; z-index:10;"></iframe>
+      <!-- BEGIN AADS AD UNIT 2449861 -->
+        <div id="frame" style="width: 100%;margin: auto;position: relative; z-index: 99998;">
+          <iframe data-aa='2449861' src='//acceptable.a-ads.com/2449861/?size=Adaptive'
+            style='border:0; padding:0; width:70%; height:auto; overflow:hidden;display: block;margin: auto'></iframe>
+        </div>
+      <!-- END AADS AD UNIT 2449861 -->
     `;
   }
 });
