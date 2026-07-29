dadb343355d1f6f5ed75490cee797fe8981dc134
ManuExplora <libertad28j2024@gmail.com>
2026-07-29 14:31:13 -0300

    style(ads): Eliminación de bordes rígidos y contenedores fijos para integración orgánica de anuncios adaptativos A-Ads

--- js/ads/banner-left.js
+++ js/ads/banner-left.js
@@ -5,12 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById('ad-sidebar-left');
   if (container) {
     container.innerHTML = `
-      <!-- BEGIN AADS AD UNIT 2449857 -->
-        <div id="frame" style="width: 100%;margin: auto;position: relative; z-index: 99998;">
-          <iframe data-aa='2449857' src='//acceptable.a-ads.com/2449857/?size=Adaptive'
-            style='border:0; padding:0; width:70%; height:auto; overflow:hidden;display: block;margin: auto'></iframe>
-        </div>
-      <!-- END AADS AD UNIT 2449857 -->
+      <!-- BEGIN AADS AD UNIT -->
+      <div style="width: 100%; margin: auto; display: flex; justify-content: center; align-items: center;">
+        <iframe data-aa='2449857' 
+                src='https://acceptable.a-ads.com/2449857/?size=Adaptive'
+                style='border:0; padding:0; width:100%; height:auto; min-height:300px; overflow:hidden; display: block; margin: auto;'
+                sandbox="allow-scripts allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-forms">
+        </iframe>
+      </div>
+      <!-- END AADS AD UNIT -->
     `;
   }
 });
